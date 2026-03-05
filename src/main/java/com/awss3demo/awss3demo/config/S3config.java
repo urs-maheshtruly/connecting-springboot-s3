041fb97f4f033b7a69c390ad5a35075f2f19717d
mahesh <maheshalladi00@gmail.com>
2026-03-05 13:35:41 +0530

    initial commit

--- src/main/java/com/awss3demo/awss3demo/config/S3config.java
+++ src/main/java/com/awss3demo/awss3demo/config/S3config.java
@@ -3,8 +3,10 @@ package com.awss3demo.awss3demo.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -12,23 +14,30 @@ import software.amazon.awssdk.services.s3.S3Client;
 @Configuration
 public class S3config {
 
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
-
-    @Value("${cloud.aws.credentials.secret-key}")
-    private String secretKey;
 
     @Value("${cloud.aws.region.static}")
     private String region;
 
     private AwsCredentialsProvider staticCredenialsProvider;
 
-    @Bean
-    public S3Client s3client(){
+    @Bean("s3Client")
+    @Profile("local")
+    public S3Client s3client(    @Value("${cloud.aws.credentials.access-key}") String accessKey ,
+                                @Value("${cloud.aws.credentials.secret-key}") String secretKey ){
+
     AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(accessKey, secretKey);
         return S3Client.builder()
                 .region(Region.of(region))
                 .credentialsProvider(StaticCredentialsProvider.create(awsBasicCredentials))
+                .build();
+    }
+
+    @Bean("s3Client")
+    @Profile("dev")
+    public S3Client s3clientDev(){
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 }
